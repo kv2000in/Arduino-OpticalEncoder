78075a9ecc1fb2dc6e8c870d37932a77b8a1c618
Kumar Vivek <kv2000in@mac.kv>
2019-03-03 19:33:21 -0500

    ISR used just to count steps. Direction established by Software. Always running at full speed

--- CNC-v0-0/CNC-v0-0.ino
+++ CNC-v0-0/CNC-v0-0.ino
@@ -1,30 +1,21 @@
 #define ENCODER0PINA         2      // interrupt pin (2,3 on nano)
-#define ENCODER0PINB         12    // any digital pin
 #define pinENA               3  //  will change
 #define pinIN1               4
 #define pinIN2               5
 
+
+int encoder0Position = 0;
+//int PPR=2520; //pulses per rotation
+int PWM = 255;
 // volatile variables - modified by interrupt service routine (ISR)
-volatile long encoder0Position = 0;
-long previousPosition = 0;
-long currentRotationCount=0; 
-long prevRotationCount=0;
-int PPR=2520; //pulses per rotation
-int fPWM = 150;//forward PWM
-int rPWM = 150;// reverse PWM
-int setRPM = 60; // to do: based on PPR - set the optimal RPM dynamically
-int readRPM = 0;
-int counter=0;
-bool PWMdetermined = false;
+volatile int counter=0;
 bool motorARunning = false;
-unsigned long perStepMicros = 0;
-unsigned long prevMicros= 0;
-unsigned long currentMicros = 0;
 //Serial Read stuff
 const byte numChars = 32;
 char receivedChars[numChars]; // an array to store the received data
 boolean newData = false;
 char DIR[32] = {0};
+//char DIR = "Z";
 int VALUE = 0;
 
 void setup()
@@ -32,31 +23,24 @@ void setup()
  
   // pins
   pinMode(ENCODER0PINA, INPUT_PULLUP);
-  pinMode(ENCODER0PINB, INPUT);
   pinMode(pinENA,OUTPUT);
   pinMode(pinIN1,OUTPUT);
   pinMode(pinIN2,OUTPUT);
   digitalWrite(pinIN1,LOW);
-  digitalWrite(pinIN2,LOW);
   //attach interrupt 
   attachInterrupt(digitalPinToInterrupt(ENCODER0PINA),onInterrupt, RISING);
   Serial.begin (9600);
   delay(1000);
-  prevMicros = micros();
 
 }
  
 void loop()
 {
-
-  //Find the optimal forward and reverse PWMs so that the RPM stays around the setRPM - so that the optical encoder works reliably. 
-  findingPWM();
- 
   //Read the Serial and move accordingly
   recvWithStartEndMarkers();
   parseData();
   //showNewData();
-  if (newData){setPosition(VALUE);}
+  if (newData){setPosition(DIR[0],VALUE);}
 
   
   
@@ -75,103 +59,20 @@ void onInterrupt()
  //For PWM <200 when interrupt is triggerred - b = 1
  // So, this would have to be individualized for different motors and encoders. 
   //int a = digitalRead(ENCODER0PINA);
-  int b = digitalRead(ENCODER0PINB);
- if(b==1)
-  {
-      encoder0Position--;
-
-  }
-  else if (b==0)
-  {
-     encoder0Position++;
-    
-  }
+  counter++;
 
 }
 
-int findPWMforSetRPM(int myPWM){
-/*call this function just once - at start up. 
-This will give us the optimal PWM for keeping the RPM at or below 600
-So that - for both directions - appropriate PWM is determined.
-*/
- 
-  while (abs(readRPM-setRPM)>20)
-  {
-    
-      whatistheRPMValue();
-  
-        //Adjust pwm every 2nd rotation
-        if (counter==2)
-          {
-              counter = 0;
-
-              if (readRPM<setRPM)
-                  {
-                      myPWM=myPWM+10;
-                      analogWrite(pinENA,myPWM);
-          
-                   }
-                else if (readRPM>setRPM)
-                   {
-                       myPWM = myPWM-10;
-                      analogWrite(pinENA,myPWM);
-                    }
-
-         
-           }
-    
-    }
-
-    //return the current PWM value
-    return myPWM;
-    
-    
-  }
-void findingPWM(){
-  if (!(PWMdetermined))
-  {
-
-    //Start the motor in reverse direction
-    rotateR();
-    Serial.print("PWM for reverse = ");
-    rPWM = findPWMforSetRPM(rPWM);
-    Serial.println(rPWM);
-    Serial.print("For RPM value = ");
-    Serial.println(readRPM);
-    //Reset the readRPM
-    readRPM=0;
-    //Start the motor in forward direction
-    rotateF();
-    Serial.print("PWM for forward = ");
-    fPWM = findPWMforSetRPM(fPWM);
-    Serial.println(fPWM);
-    Serial.print("For RPM value = ");
-    Serial.println(readRPM);
-    //Reset the readRPM
-    readRPM=0;
-
-    PWMdetermined = true;
-    //turn off the motor for now
-    rotateStop();
-    //reset the position counter (will add endstop logic here)
-    currentRotationCount=0;
-      //Once optimal speed of rotation has been established - now zero the encoder position (will need some ENDstop logic)
-    previousPosition=0;
-    encoder0Position = 0;
-     }
-  
-  }
-
 void rotateF()
 {
-    analogWrite(pinENA, fPWM);
+    analogWrite(pinENA, PWM);
     digitalWrite(pinIN1,HIGH);
     digitalWrite(pinIN2,LOW);
     motorARunning=true;
 }
 void rotateR()
 {
-    analogWrite(pinENA, rPWM);
+    analogWrite(pinENA, PWM);
     digitalWrite(pinIN1,LOW);
     digitalWrite(pinIN2,HIGH);
     motorARunning=true;
@@ -181,27 +82,7 @@ void rotateStop()
   analogWrite(pinENA, 0);
   motorARunning=false;
 }
-void whatistheRPMValue()
-  {
-    // only display position info if has changed
-     if (encoder0Position != previousPosition )
-        {
 
-            currentRotationCount=encoder0Position/PPR;
-            previousPosition = encoder0Position;
-  
-          }
-      if (currentRotationCount != prevRotationCount)
-          {
-             currentMicros = micros();
-             perStepMicros = currentMicros - prevMicros;
-             prevMicros=currentMicros;
-             readRPM =(1000000/perStepMicros)*60;
-             counter++;
-             prevRotationCount=currentRotationCount;
-    
-          }
-    }
 //Serial Read Functions
 void recvWithStartEndMarkers() {
     static boolean recvInProgress = false;
@@ -264,25 +145,37 @@ void parseData() {
   VALUE = atoi(strtokIndx+2);   
  }
 
-void setPosition(int pos)
+void setPosition(char dir,int steps)
 {
   //This code causes some end jitters - as in either direction - there is a slight overshoot and then it goes back and forth until position is reached.
   //Also - if the encoder0Position changes - by external movement or whatever - this tries to bring it back to the pos parameter
   //I have set the RPM to 60 but this gives very little torque. Less jitters at slower speed as chances of overshooting are less.
   //Added newData if logic to only call it once per new data received.
-  while (encoder0Position!=pos)
-  {
-   if  (encoder0Position<pos)
+
+    while (steps>counter)
     {
-      if (not (motorARunning)) {rotateF();}
-    }
-   else if ((encoder0Position>pos)) 
-    {
-      if (not (motorARunning)) {rotateR();}
-    } 
-   }
-   //position reached - now stop 
-   if (motorARunning){rotateStop();}
-   newData = false;
-   
+         if (dir=='F') //Single quotes - Double quotes don't work.
+            {
+                if (not (motorARunning)) {rotateF();}
+             }
+         else if (dir=='R')
+            {
+               if (not (motorARunning)) {rotateR();}
+            }
+     }
+    if (motorARunning){rotateStop();}
+             if (dir=='F')
+            {
+                encoder0Position = encoder0Position+counter;
+             }
+         else if (dir=='R')
+            {
+               encoder0Position = encoder0Position-counter;
+            }
+    newData = false;
+    counter =0;
+    Serial.print("Encoder position = ");
+    Serial.println(encoder0Position);
+        
+    
 }

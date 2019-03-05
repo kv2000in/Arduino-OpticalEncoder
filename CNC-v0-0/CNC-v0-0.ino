0c6d6fd65e0ca6fe65e530fef026e81f69e688ba
Kumar Vivek <kv2000in@mac.kv>
2019-03-04 19:51:16 -0500

    Dual encoders

--- CNC-v0-0/CNC-v0-0.ino
+++ CNC-v0-0/CNC-v0-0.ino
@@ -1,14 +1,17 @@
-#define ENCODER0PINA         2      // interrupt pin (2,3 on nano)
+#define ENCODER0PINA         2     // interrupt pin (2,3 on nano)
+#define ENCODER1PINA         3     // interrupt pin (2,3 on nano)
 #define pinENA               6
 #define pinIN1               4
 #define pinIN2               5
 
 
 long encoder0Position = 0;
+long encoder1Position = 0;
 //int PPR=2520; //pulses per rotation
 int PWM = 255;
 // volatile variables - modified by interrupt service routine (ISR)
-volatile long counter=0;
+volatile long counter0=0;
+volatile long counter1=0;
 bool motorARunning = false;
 //Serial Read stuff
 const byte numChars = 32;
@@ -24,12 +27,14 @@ void setup()
  
   // pins
   pinMode(ENCODER0PINA, INPUT_PULLUP);
+  pinMode(ENCODER1PINA, INPUT_PULLUP);
   pinMode(pinENA,OUTPUT);
   pinMode(pinIN1,OUTPUT);
   pinMode(pinIN2,OUTPUT);
 
-  //attach interrupt 
-  attachInterrupt(digitalPinToInterrupt(ENCODER0PINA),onInterrupt, RISING);
+  //attach interrupts 
+  attachInterrupt(digitalPinToInterrupt(ENCODER0PINA),onInterrupt0, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCODER1PINA),onInterrupt1, RISING); 
   Serial.begin (9600);
   //?Intialize the PWM??
   analogWrite(pinENA, 0);
@@ -55,7 +60,7 @@ void loop()
 }
 // Minimalistic ISR
 
-void onInterrupt()
+void onInterrupt0()
 {
   // read both inputs - Why?? Interrupt was triggered by RISING edge on a
   // So we know that a =1 and that should be the case always.
@@ -67,10 +72,14 @@ void onInterrupt()
  //For PWM <200 when interrupt is triggerred - b = 1
  // So, this would have to be individualized for different motors and encoders. 
   //int a = digitalRead(ENCODER0PINA);
-  counter++;
+  counter0++;
 
 }
+void onInterrupt1()
+{
+  counter1++;
 
+}
 void rotateF()
 {
     analogWrite(pinENA, PWM);
@@ -159,7 +168,7 @@ void setPosition(char dir, long steps)
   //During the first run @ full speed (PWM 255, 24V PSU) - Some interrupts are missed probably - and the rotation is slightly more than (I think) the number of steps
   //Subsequent repeated  calls to this function yields accurate positions. May be something to do with function initialization
     
-    if (steps>counter)
+    if (steps>counter0)
     {
          if (dir=='F') //Single quotes - Double quotes don't work.
             {
@@ -177,18 +186,23 @@ void setPosition(char dir, long steps)
              rotateStop();
              if (dir=='F')
             {
-                encoder0Position = encoder0Position+counter;
+                encoder0Position = encoder0Position+counter0;
+                encoder1Position = encoder1Position+counter1;
              }
          else if (dir=='R')
             {
-               encoder0Position = encoder0Position-counter;
+               encoder0Position = encoder0Position-counter0;
+               encoder1Position = encoder1Position+counter1;
             }
         
-        counter =0;
+        counter0 =0;
+        counter1 =0;
 
             }
       newData = false;
-      Serial.print("Encoder is at");
+      Serial.print("Encoder 0 is at ");
       Serial.println(encoder0Position); 
+      Serial.print("Encoder 1 is at ");
+      Serial.println(encoder1Position); 
      }
 }
